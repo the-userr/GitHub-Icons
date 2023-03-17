404375887fa27cceb6e65e91ece4bea1e7769860
the-userr <888levant888@gmail.com>
2023-03-18 01:32:25 +0600

    Renamed .txt-files Made compile.cmd workable and also deleted UserCSS_original.css

--- compile.cmd
+++ compile.cmd
@@ -1,14 +1,36 @@
-@ECHO OFF>nul
+@ECHO OFF>NUL
+PUSHD %~dp0
+SETLOCAL EnableDelayedExpansion
 
-(
-	TYPE meta.txt
-	TYPE header.txt
-	TYPE main.txt
-	TYPE footer.txt
-) > UserCSS.css
+ECHO WARNING: this script will rewrite "\UserCSS.css".
+ECHO ("%cd%\UserCSS.css")
+:choice
+SET /p "choice=Do you want to continue [Y,N]? "
+IF /I "%choice%" == "N" (EXIT /B)
+IF /I NOT "%choice%" == "Y" (
+IF /I NOT "%choice%" == "N" (GOTO choice)
+)
+ECHO.
 
-EXTENSION = .md
-EXTENSION_VAR = markdown
+ECHO Creating "\data\UserCSS-temp.css"...
+TYPE "%cd%\data\header.txt" > "%cd%\data\UserCSS-temp.css"
+FOR /F "usebackq skip=1 tokens=1,2,3 delims=;" %%A IN ("%cd%\data\filenames.txt") DO (
+	FOR /F "usebackq delims=" %%j IN (`TYPE "%cd%\data\selectors.css"`) DO (
+		SET string=%%j
+		SET string=!string:$filename$=%%A!
+		IF "%%B" == "file" 		(SET string=!string:$type$=File!)
+		IF "%%B" == "filemask" 	(SET string=!string:$type$=File!)
+		IF "%%B" == "dir" 		(SET string=!string:$type$=Directory!)
+		SET string=!string:$CSS_var$=%%C!
+		ECHO !string!>> "%cd%\data\UserCSS-temp.css"
+	)
+	ECHO.>> "%cd%\data\UserCSS-temp.css"
+)
+TYPE "%cd%\data\footer.txt" >> "%cd%\data\UserCSS-temp.css"
 
-
-PAUSE
+ECHO Moving "\data\UserCSS-temp.css" to "\UserCSS.css"...
+MOVE "%cd%\data\UserCSS-temp.css" "%cd%\UserCSS.css"
+ENDLOCAL
+POPD
+ECHO Done!
+EXIT /B
