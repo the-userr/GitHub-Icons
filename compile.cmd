cff5c06a10549d24f700157319b884e4abce938a
the-userr <888levant888@gmail.com>
2023-03-27 03:23:53 +0600

    Renamed UserCSS file

--- compile.cmd
+++ compile.cmd
@@ -2,8 +2,8 @@
 PUSHD %~dp0
 SETLOCAL EnableDelayedExpansion
 
-ECHO WARNING: this script will rewrite "\UserCSS.css".
-ECHO ("%cd%\UserCSS.css")
+ECHO WARNING: this script will rewrite "\UserCSS.user.css".
+ECHO ("%cd%\UserCSS.user.css")
 :choice
 SET /p "choice=Do you want to continue [Y,N]? "
 IF /I "%choice%" == "N" (EXIT /B)
@@ -13,7 +13,7 @@ IF /I NOT "%choice%" == "N" (GOTO choice)
 ECHO.
 
 ECHO Creating "\data\UserCSS-temp.css"...
-TYPE "%cd%\data\header.txt" > "%cd%\data\UserCSS-temp.css"
+TYPE "%cd%\data\header.txt" > "%cd%\data\UserCSS-temp.user.css"
 FOR /F "usebackq skip=1 tokens=1,2,3 delims=;" %%A IN ("%cd%\data\filenames.txt") DO (
 	FOR /F "usebackq delims=" %%j IN (`TYPE "%cd%\data\selectors.css"`) DO (
 		SET string=%%j
@@ -22,14 +22,14 @@ FOR /F "usebackq skip=1 tokens=1,2,3 delims=;" %%A IN ("%cd%\data\filenames.txt"
 		IF "%%B" == "filemask" 	(SET string=!string:$type$=File!)
 		IF "%%B" == "dir" 		(SET string=!string:$type$=Directory!)
 		SET string=!string:$CSS_var$=%%C!
-		ECHO !string!>> "%cd%\data\UserCSS-temp.css"
+		ECHO !string!>> "%cd%\data\UserCSS-temp.user.css"
 	)
-	ECHO.>> "%cd%\data\UserCSS-temp.css"
+	ECHO.>> "%cd%\data\UserCSS-temp.user.css"
 )
-TYPE "%cd%\data\footer.txt" >> "%cd%\data\UserCSS-temp.css"
+TYPE "%cd%\data\footer.txt" >> "%cd%\data\UserCSS-temp.user.css"
 
-ECHO Moving "\data\UserCSS-temp.css" to "\UserCSS.css"...
-MOVE "%cd%\data\UserCSS-temp.css" "%cd%\UserCSS.css"
+ECHO Moving "\data\UserCSS-temp.user.css" to "\UserCSS.user.css"...
+MOVE "%cd%\data\UserCSS-temp.user.css" "%cd%\UserCSS.user.css"
 ENDLOCAL
 POPD
 ECHO Done!
